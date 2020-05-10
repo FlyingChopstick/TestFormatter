f08a99994cf2ae9f63354b0a85395d4160d96ef6
Alexander Yakimov <1flyingchopstick1@gmail.com>
2020-05-10 17:14:59 +0300

    Generator completed

---
+++ TestFormatterUI/TestFormatter.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace TestFormatterUI
+{
+    partial class TestFormatter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cb_answers = new System.Windows.Forms.GroupBox();
+            this.rb_ans4 = new System.Windows.Forms.RadioButton();
+            this.rb_ans3 = new System.Windows.Forms.RadioButton();
+            this.rb_ans2 = new System.Windows.Forms.RadioButton();
+            this.rb_ans1 = new System.Windows.Forms.RadioButton();
+            this.tb_ans3 = new System.Windows.Forms.TextBox();
+            this.tb_ans4 = new System.Windows.Forms.TextBox();
+            this.tb_ans2 = new System.Windows.Forms.TextBox();
+            this.tb_ans1 = new System.Windows.Forms.TextBox();
+            this.tb_question = new System.Windows.Forms.TextBox();
+            this.tb_header = new System.Windows.Forms.TextBox();
+            this.l_header = new System.Windows.Forms.Label();
+            this.l_question = new System.Windows.Forms.Label();
+            this.b_generate = new System.Windows.Forms.Button();
+            this.l_topic = new System.Windows.Forms.Label();
+            this.tb_topic = new System.Windows.Forms.TextBox();
+            this.cb_answers.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cb_answers
+            // 
+            this.cb_answers.Controls.Add(this.rb_ans4);
+            this.cb_answers.Controls.Add(this.rb_ans3);
+            this.cb_answers.Controls.Add(this.rb_ans2);
+            this.cb_answers.Controls.Add(this.rb_ans1);
+            this.cb_answers.Controls.Add(this.tb_ans3);
+            this.cb_answers.Controls.Add(this.tb_ans4);
+            this.cb_answers.Controls.Add(this.tb_ans2);
+            this.cb_answers.Controls.Add(this.tb_ans1);
+            this.cb_answers.Location = new System.Drawing.Point(9, 147);
+            this.cb_answers.Name = "cb_answers";
+            this.cb_answers.Size = new System.Drawing.Size(739, 191);
+            this.cb_answers.TabIndex = 0;
+            this.cb_answers.TabStop = false;
+            this.cb_answers.Text = "Answers";
+            // 
+            // rb_ans4
+            // 
+            this.rb_ans4.AutoSize = true;
+            this.rb_ans4.Location = new System.Drawing.Point(18, 150);
+            this.rb_ans4.Name = "rb_ans4";
+            this.rb_ans4.Size = new System.Drawing.Size(14, 13);
+            this.rb_ans4.TabIndex = 7;
+            this.rb_ans4.UseVisualStyleBackColor = true;
+            // 
+            // rb_ans3
+            // 
+            this.rb_ans3.AutoSize = true;
+            this.rb_ans3.Location = new System.Drawing.Point(18, 112);
+            this.rb_ans3.Name = "rb_ans3";
+            this.rb_ans3.Size = new System.Drawing.Size(14, 13);
+            this.rb_ans3.TabIndex = 6;
+            this.rb_ans3.UseVisualStyleBackColor = true;
+            // 
+            // rb_ans2
+            // 
+            this.rb_ans2.AutoSize = true;
+            this.rb_ans2.Location = new System.Drawing.Point(18, 73);
+            this.rb_ans2.Name = "rb_ans2";
+            this.rb_ans2.Size = new System.Drawing.Size(14, 13);
+            this.rb_ans2.TabIndex = 5;
+            this.rb_ans2.UseVisualStyleBackColor = true;
+            // 
+            // rb_ans1
+            // 
+            this.rb_ans1.AutoSize = true;
+            this.rb_ans1.Checked = true;
+            this.rb_ans1.Location = new System.Drawing.Point(18, 36);
+            this.rb_ans1.Name = "rb_ans1";
+            this.rb_ans1.Size = new System.Drawing.Size(14, 13);
+            this.rb_ans1.TabIndex = 4;
+            this.rb_ans1.TabStop = true;
+            this.rb_ans1.UseVisualStyleBackColor = true;
+            // 
+            // tb_ans3
+            // 
+            this.tb_ans3.Location = new System.Drawing.Point(38, 109);
+            this.tb_ans3.Name = "tb_ans3";
+            this.tb_ans3.Size = new System.Drawing.Size(695, 20);
+            this.tb_ans3.TabIndex = 3;
+            // 
+            // tb_ans4
+            // 
+            this.tb_ans4.Location = new System.Drawing.Point(38, 147);
+            this.tb_ans4.Name = "tb_ans4";
+            this.tb_ans4.Size = new System.Drawing.Size(695, 20);
+            this.tb_ans4.TabIndex = 2;
+            // 
+            // tb_ans2
+            // 
+            this.tb_ans2.Location = new System.Drawing.Point(38, 70);
+            this.tb_ans2.Name = "tb_ans2";
+            this.tb_ans2.Size = new System.Drawing.Size(695, 20);
+            this.tb_ans2.TabIndex = 1;
+            // 
+            // tb_ans1
+            // 
+            this.tb_ans1.Location = new System.Drawing.Point(38, 33);
+            this.tb_ans1.Name = "tb_ans1";
+            this.tb_ans1.Size = new System.Drawing.Size(695, 20);
+            this.tb_ans1.TabIndex = 0;
+            // 
+            // tb_question
+            // 
+            this.tb_question.Location = new System.Drawing.Point(12, 121);
+            this.tb_question.Name = "tb_question";
+            this.tb_question.Size = new System.Drawing.Size(730, 20);
+            this.tb_question.TabIndex = 8;
+            // 
+            // tb_header
+            // 
+            this.tb_header.Location = new System.Drawing.Point(12, 77);
+            this.tb_header.Name = "tb_header";
+            this.tb_header.Size = new System.Drawing.Size(730, 20);
+            this.tb_header.TabIndex = 9;
+            // 
+            // l_header
+            // 
+            this.l_header.AutoSize = true;
+            this.l_header.Location = new System.Drawing.Point(9, 61);
+            this.l_header.Name = "l_header";
+            this.l_header.Size = new System.Drawing.Size(42, 13);
+            this.l_header.TabIndex = 10;
+            this.l_header.Text = "Header";
+            // 
+            // l_question
+            // 
+            this.l_question.AutoSize = true;
+            this.l_question.Location = new System.Drawing.Point(9, 100);
+            this.l_question.Name = "l_question";
+            this.l_question.Size = new System.Drawing.Size(49, 13);
+            this.l_question.TabIndex = 11;
+            this.l_question.Text = "Question";
+            // 
+            // b_generate
+            // 
+            this.b_generate.Location = new System.Drawing.Point(9, 344);
+            this.b_generate.Name = "b_generate";
+            this.b_generate.Size = new System.Drawing.Size(75, 23);
+            this.b_generate.TabIndex = 12;
+            this.b_generate.Text = "Generate";
+            this.b_generate.UseVisualStyleBackColor = true;
+            this.b_generate.Click += new System.EventHandler(this.b_generate_Click);
+            // 
+            // l_topic
+            // 
+            this.l_topic.AutoSize = true;
+            this.l_topic.Location = new System.Drawing.Point(9, 6);
+            this.l_topic.Name = "l_topic";
+            this.l_topic.Size = new System.Drawing.Size(63, 13);
+            this.l_topic.TabIndex = 15;
+            this.l_topic.Text = "Topic name";
+            // 
+            // tb_topic
+            // 
+            this.tb_topic.Location = new System.Drawing.Point(12, 22);
+            this.tb_topic.Name = "tb_topic";
+            this.tb_topic.Size = new System.Drawing.Size(730, 20);
+            this.tb_topic.TabIndex = 14;
+            // 
+            // TestFormatter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 373);
+            this.Controls.Add(this.l_topic);
+            this.Controls.Add(this.tb_topic);
+            this.Controls.Add(this.b_generate);
+            this.Controls.Add(this.l_question);
+            this.Controls.Add(this.l_header);
+            this.Controls.Add(this.tb_question);
+            this.Controls.Add(this.tb_header);
+            this.Controls.Add(this.cb_answers);
+            this.Name = "TestFormatter";
+            this.Text = "TestFormatter";
+            this.cb_answers.ResumeLayout(false);
+            this.cb_answers.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox cb_answers;
+        private System.Windows.Forms.RadioButton rb_ans4;
+        private System.Windows.Forms.RadioButton rb_ans3;
+        private System.Windows.Forms.RadioButton rb_ans2;
+        private System.Windows.Forms.RadioButton rb_ans1;
+        private System.Windows.Forms.TextBox tb_ans3;
+        private System.Windows.Forms.TextBox tb_ans4;
+        private System.Windows.Forms.TextBox tb_ans2;
+        private System.Windows.Forms.TextBox tb_ans1;
+        private System.Windows.Forms.TextBox tb_question;
+        private System.Windows.Forms.TextBox tb_header;
+        private System.Windows.Forms.Label l_header;
+        private System.Windows.Forms.Label l_question;
+        private System.Windows.Forms.Button b_generate;
+        private System.Windows.Forms.Label l_topic;
+        private System.Windows.Forms.TextBox tb_topic;
+    }
+}
---
+++ TestFormatterUI/TestFormatter.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestFormatterUI
+{
+    public partial class TestFormatter : Form
+    {
+        private const string folder = @".\Topics";
+
+        public TestFormatter()
+        {
+            Directory.CreateDirectory(folder);
+            InitializeComponent();
+        }
+
+        private void Generate()
+        {
+            string header = $"I: {tb_header.Text}; mt=0,1";
+            string question = $"S: {tb_question.Text}";
+            string ans1, ans2, ans3, ans4;
+
+            //ans1
+            if (rb_ans1.Checked)
+            {
+                ans1 = $"+: {tb_ans1.Text}";
+            }
+            else
+            {
+                ans1 = $"-: {tb_ans1.Text}";
+            }
+
+            //ans2
+            if (rb_ans2.Checked)
+            {
+                ans2 = $"+: {tb_ans2.Text}";
+            }
+            else
+            {
+                ans2 = $"-: {tb_ans2.Text}";
+            }
+
+            //ans3
+            if (rb_ans3.Checked)
+            {
+                ans3 = $"+: {tb_ans3.Text}";
+            }
+            else
+            {
+                ans3 = $"-: {tb_ans3.Text}";
+            }
+
+            //ans4
+            if (rb_ans4.Checked)
+            {
+                ans4 = $"+: {tb_ans4.Text}";
+            }
+            else
+            {
+                ans4 = $"-: {tb_ans4.Text}";
+            }
+
+            //compile
+            string[] result = { header, question, ans1, ans2, ans3, ans4, "" };
+            string fileName = $".\\{folder}\\{tb_topic.Text}.txt";
+
+            //write
+            File.AppendAllLines(fileName, result);
+        }
+
+        private void b_generate_Click(object sender, EventArgs e)
+        {
+            if (tb_topic.Text != "")
+            {
+                Generate();
+
+                tb_header.Clear();
+                tb_question.Clear();
+                tb_ans1.Clear();
+                tb_ans2.Clear();
+                tb_ans3.Clear();
+                tb_ans4.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Fill the topic", "Topic", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
